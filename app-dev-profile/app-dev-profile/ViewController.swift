b7459a94935c9beeb4e5852edf9670e1f02c2b0f
Optimix Designer <optimix.designer@gmail.com>
2017-08-15 03:07:29 +0300

    initial xcode project

---
+++ app-dev-profile/app-dev-profile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app-dev-profile
+//
+//  Created by Alexander on 15.08.17.
+//  Copyright © 2017 Alexander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
